0b51e400910a7553e753c7c40f6b927ef0db62e9
Cameron Bernhardt <cambernhardt@me.com>
2020-02-10 03:32:42 -0500

    Do smart file-type detection to fix Facebook's ext hardcoding

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const botcore = require("messenger-botcore");
-const request = require("request");
 const fs = require("fs");
+const request = require("request");
 const FileType = require("file-type");
+const TinyURL = require("tinyurl");
 const config = require("./config");
 const server = require("./server");
 
@@ -27,7 +28,7 @@ function listener(err, msg) {
         && msg.attachments && msg.attachments) {
         // Also pull user information for attribution
         const audios = msg.attachments.filter(m => m.type == "audio");
-        console.log(audios);
+
         if (audios.length > 0) {
             api.getUserInfo(msg.senderID, (err, info) => {
                 if (!err) {
@@ -46,20 +47,37 @@ function listener(err, msg) {
 
 function downloadFile(author, url, filename) {
     const userDir = `${__dirname}/audios/${author}`;
-    const fullPath = `${userDir}/${filename}`;
 
     // Create directory for a user's recordings if it doesn't yet exist
     if (!fs.existsSync(userDir)) {
         fs.mkdirSync(userDir);
     }
 
-    request(url).pipe(fs.createWriteStream(fullPath)).on('close', (err, data) => {
+    const fullPath = `${userDir}/${filename}`;
+    request(url).pipe(fs.createWriteStream(fullPath)).on('close', async (err, data) => {
         console.log(`Downloaded ${filename}.`);
         
-        const url = `http://${config.SERVER_URL}:${config.PORT}${config.URL_BASE}/${author}/${filename}`;
-        api.sendMessage({
-            body: url,
-            url: url
-        }, config.THREAD_ID);
+        // Read file to determine true file type (not extension reported by FB)
+        const calcFileType = await FileType.fromFile(fullPath);
+        let fixedFileName = filename;
+        if (calcFileType && calcFileType.ext) {
+            const reportedFileType = filename.match(/^([^.]+)\.(.+)$/);
+            const fileStub = (reportedFileType && reportedFileType[2]) ? reportedFileType[1] : filename;
+
+            // If FB isn't giving us an ext, or the ext they're giving us doesn't match the true ext,
+            // rename the file with the proper one
+            if (!reportedFileType || (reportedFileType[2] && (reportedFileType[2] != calcFileType.ext))) {
+                fixedFileName = `${fileStub}.${calcFileType.ext}`;
+                fs.renameSync(fullPath, `${userDir}/${fixedFileName}`);
+            }
+        }
+        
+        const url = `http://${config.SERVER_URL}:${config.PORT}${config.URL_BASE}/${author}/${fixedFileName}`;
+        TinyURL.shorten(url, (res, err) => { // Why does the TinyURL API have the params in this order?
+            api.sendMessage({
+                body: err ? url : res,
+                url: url
+            }, config.THREAD_ID);
+        });
     });
 }
