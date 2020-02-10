6951508e39a7621e5caed77b8243e60f41f39c88
Cameron Bernhardt <cambernhardt@me.com>
2020-02-10 02:58:29 -0500

    Add HTML web player links

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const botcore = require("messenger-botcore");
 const request = require("request");
 const fs = require("fs");
+const FileType = require("file-type");
 const config = require("./config");
 const server = require("./server");
 
-let gapi;
+let api;
 
 botcore.login.login({
     MEMCACHIER_USERNAME: process.env.MEMCACHIER_USERNAME,
     MEMCACHIER_PASSWORD: process.env.MEMCACHIER_PASSWORD,
     MEMCACHIER_SERVERS: process.env.MEMCACHIER_SERVERS
-}, (err, api) => {
+}, (err, apiInstance) => {
     if (!err) {
-        gapi = api;
+        api = apiInstance;
         api.setOptions({ listenEvents: true });
         api.listenMqtt(listener);
     } else {
@@ -24,22 +25,41 @@ botcore.login.login({
 function listener(err, msg) {
     if (!err && msg.threadID && msg.threadID == config.THREAD_ID
         && msg.attachments && msg.attachments) {
+        // Also pull user information for attribution
         const audios = msg.attachments.filter(m => m.type == "audio");
+        console.log(audios);
+        if (audios.length > 0) {
+            api.getUserInfo(msg.senderID, (err, info) => {
+                if (!err) {
+                    const userName = info[msg.senderID].firstName;
 
-        audios.forEach(audio => {
-            downloadFile(audio.url, audio.filename);
-        });
+                    audios.forEach(audio => {
+                        downloadFile(userName, audio.url, audio.filename);
+                    });
+                } else {
+                    api.sendMessage("Sorry, couldn't retrieve author info for download.", msg.threadID);
+                }
+            });
+        }
     }
 }
 
-function downloadFile(url, filename) {
-    const fullpath = `${__dirname}/audios/${filename}`;
+function downloadFile(author, url, filename) {
+    const userDir = `${__dirname}/audios/${author}`;
+    const fullPath = `${userDir}/${filename}`;
 
-    request(url).pipe(fs.createWriteStream(fullpath)).on('close', (err, data) => {
+    // Create directory for a user's recordings if it doesn't yet exist
+    if (!fs.existsSync(userDir)) {
+        fs.mkdirSync(userDir);
+    }
+
+    request(url).pipe(fs.createWriteStream(fullPath)).on('close', (err, data) => {
         console.log(`Downloaded ${filename}.`);
-
-        gapi.sendMessage({
-            url: `http://${config.SERVER_URL}:${config.PORT}${config.URL_BASE}/${filename}`
+        
+        const url = `http://${config.SERVER_URL}:${config.PORT}${config.URL_BASE}/${author}/${filename}`;
+        api.sendMessage({
+            body: url,
+            url: url
         }, config.THREAD_ID);
     });
 }
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const config = require("./config");
 
 app.set("port", config.PORT);
+app.set("view engine", "pug");
 app.listen(app.get("port"));
 
 // Serve audio files from the folder
-app.get(`${config.URL_BASE}/:name`, (req, res) => {
-    const name = req.params.name;
-    res.sendFile(`${__dirname}/audios/${name}`);
+app.get(`/audios/:author/:clip`, (req, res) => {
+    const author = req.params.author;
+    const clip = req.params.clip;
+    const path = `${__dirname}/audios/${author}/${clip}`
+
+    fs.access(path, err => {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            res.sendFile(path);
+        }
+    });
+});
+
+// Render a simple web player to play audio in browser
+app.get(`${config.URL_BASE}/:author/:clip`, (req, res) => {
+    const author = req.params.author;
+    const clip = req.params.clip;
+    const path = `/audios/${author}/${clip}`;
+
+    fs.access(`${__dirname}/${path}`, err => {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            res.render("index", {
+                "author": author,
+                "path": path
+            });
+        }
+    });
 });
